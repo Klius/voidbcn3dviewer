93ec0e9181f45d19f1f0741ba24103596f204d2f
klius <kliusescarlopini@gmail.com>
2019-06-26 14:02:22 +0200

    añadidos controles FPS,falta pulir mierders

--- penguin-engine.js
+++ penguin-engine.js
@@ -5,6 +5,16 @@ var group;
 var computer = {on:false}
 var videos = [];
 var vtexs = [];
+//Controls
+var moveForward = false;
+var moveBackward = false;
+var moveLeft = false;
+var moveRight = false;
+var prevTime = performance.now();
+var velocity = new THREE.Vector3();
+var direction = new THREE.Vector3();
+var vertex = new THREE.Vector3();
+
 function init(){
     scene = new THREE.Scene();
     scene.background = new THREE.Color( 0x000000 );
@@ -24,13 +34,69 @@ function init(){
     scene.add( gridHelper );
     //CONTROLS
     document.getElementById("viewport").appendChild( renderer.domElement );
-    controls = new THREE.OrbitControls(camera,renderer.domElement);
-    controls.autoRotate = false;
-    controls.enablePan = false;
+    controls = new THREE.PointerLockControls( camera );//new THREE.OrbitControls(camera,renderer.domElement);
+    document.getElementById("viewport").addEventListener( 'click', function () {
+        controls.lock();
+    }, false );
+    controls.addEventListener( 'lock', function () {
+    } );
+    controls.addEventListener( 'unlock', function () {
+
+    } );
+    scene.add(controls.getObject());
+    var onKeyDown = function ( event ) {
+        switch ( event.keyCode ) {
+            case 38: // up
+            case 87: // w
+                moveForward = true;
+                break;
+            case 37: // left
+            case 65: // a
+                moveLeft = true;
+                break;
+            case 40: // down
+            case 83: // s
+                moveBackward = true;
+                break;
+            case 39: // right
+            case 68: // d
+                moveRight = true;
+                break;
+            case 32: // space
+                if ( canJump === true ) velocity.y += 350;
+                canJump = false;
+                break;
+        }
+    };
+    var onKeyUp = function ( event ) {
+        switch ( event.keyCode ) {
+            case 38: // up
+            case 87: // w
+                moveForward = false;
+                break;
+            case 37: // left
+            case 65: // a
+                moveLeft = false;
+                break;
+            case 40: // down
+            case 83: // s
+                moveBackward = false;
+                break;
+            case 39: // right
+            case 68: // d
+                moveRight = false;
+                break;
+        }
+    };
+    document.addEventListener( 'keydown', onKeyDown, false );
+    document.addEventListener( 'keyup', onKeyUp, false );
+
+    // controls.autoRotate = false;
+    //controls.enablePan = false;
     //controls.maxDistance = 25;
     //controls.minDistance = 15;
     //controls.maxPolarAngle = 1.5;
-    controls.update();
+    //controls.update();
     //VIDEOTEXTURE
     loadVideoTextures()
     /*LIGHTS*/
@@ -109,13 +175,46 @@ function onWindowResize() {
 function animate() {
     requestAnimationFrame( animate );
     //animation stuff goes here
-    controls.update();
+    move();
     directionalLight.position.copy( camera.position );
     renderer.render( scene, camera );
 }
+function move(){
+    if ( controls.isLocked === true ) {
+        console.log(moveForward);
+        var objects = [];
+        raycaster.ray.origin.copy( controls.getObject().position );
+        raycaster.ray.origin.y -= 10;
+        var intersections = raycaster.intersectObjects( objects );
+        var onObject = intersections.length > 0;
+        var time = performance.now();
+        var delta = ( time - prevTime ) / 1000;
+        velocity.x -= velocity.x * 10.0 * delta;
+        velocity.z -= velocity.z * 10.0 * delta;
+        velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
+        direction.z = Number( moveForward ) - Number( moveBackward );
+        direction.x = Number( moveLeft ) - Number( moveRight );
+        direction.normalize(); // this ensures consistent movements in all directions
+        if ( moveForward || moveBackward ) velocity.z -= direction.z * 100.0 * delta;
+        if ( moveLeft || moveRight ) velocity.x -= direction.x * 100.0 * delta;
+        if ( onObject === true ) {
+            velocity.y = Math.max( 0, velocity.y );
+            canJump = true;
+        }
+        controls.getObject().translateX( velocity.x * delta );
+        controls.getObject().position.y += ( velocity.y * delta ); // new behavior
+        controls.getObject().translateZ( velocity.z * delta );
+        if ( controls.getObject().position.y < 1 ) {
+            velocity.y = 0;
+            controls.getObject().position.y = 1;
+            canJump = true;
+        }
+        prevTime = time;
+    }
+}
 function loadScene(){
     loader.load(
-        "models/monitor-test.glb",
+        "http://127.0.0.1:8000/models/monitor-test.glb", //models/monitor-test.glb",
         function(gltf){
             //gltf.scene.name = objects[obj].name;
             gltf.scene.traverse( ( o ) => {
